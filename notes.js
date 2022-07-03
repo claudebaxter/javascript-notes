b7ee36ac331c31e59ba4319fc5fe764d25a6371a
elborracho420 <e.baxter@dark-coin.io>
2022-07-03 15:53:14 -0500

    Creating multiple paths by chaining if/else pairs. Added index.html so users can view developer tools console to see results of inputs.

--- notes.js
+++ notes.js
@@ -2,7 +2,8 @@
 
 let theNumber = Number(prompt("pick a number"));
 if (!Number.isNaN(theNumber)) {
-  console.log("Your number is the square root of")
+  console.log("Your number is the square root of" +
+              theNumber * theNumber);
 } else {
   console.log("Hey. Why didn't you give me a number?");
 }
@@ -10,3 +11,17 @@ if (!Number.isNaN(theNumber)) {
 //The if keyword executes or skips a statement depending//
 //on the value of a Boolean expression, followed by the//
 //statement to execute://
+
+//For more than two paths, you can chain multiple if/else pairs//
+
+let num = Number(prompt("Pick a number"));
+
+if (num < 10) {
+  console.log("Small");
+} else if (num <100) {
+  console.log("Medium");
+} else {
+  console.log("Large");
+}
+
+//0-9 returns Small, 10-99 returns Medium, 100+ returns Large//
