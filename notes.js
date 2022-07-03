51a59f39043f907832849faf11bc3eddc8b4c65f
elborracho420 <e.baxter@dark-coin.io>
2022-07-03 14:58:39 -0500

    Conditional execution example.

---
+++ notes.js
@@ -0,0 +1,12 @@
+// conditional execution //
+
+let theNumber = Number(prompt("pick a number"));
+if (!Number.isNaN(theNumber)) {
+  console.log("Your number is the square root of")
+} else {
+  console.log("Hey. Why didn't you give me a number?");
+}
+
+//The if keyword executes or skips a statement depending//
+//on the value of a Boolean expression, followed by the//
+//statement to execute://
